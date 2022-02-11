2a2cd878975338acb4dcdc8337f7ae3ffbbec0eb
HyeonjuLee <l67378810@gmail.com>
2022-02-11 09:43:40 +0900

    feat(payment): Implement Payment Class

---
+++ src/main/java/lotto/domain/Payment.java
@@ -0,0 +1,21 @@
+package lotto.domain;
+
+public class Payment {
+    public static final int LOTTO_PRICE = 1000;
+
+    public final int paymentAmount;
+    public final int numberOfPurchases;
+
+    public Payment(final int paymentAmount) {
+        isAvailableToPurchase(paymentAmount);
+        this.paymentAmount = paymentAmount;
+        numberOfPurchases = paymentAmount / LOTTO_PRICE;
+    }
+
+    private void isAvailableToPurchase(final int paymentAmount) {
+        if (paymentAmount < LOTTO_PRICE) {
+            throw new IllegalArgumentException("[ERROR] 구매할 수 있는 최소 금액보다 작습니다.");
+        }
+    }
+
+}
